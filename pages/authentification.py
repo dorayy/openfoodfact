fb213a4d4b38276263c70422d4587d7af5c617ea
NicolasDENIS <n.denis@ecole-ipssi.net>
2022-09-23 09:48:40 +0200

    @dev: Adding registration

--- pages/authentification.py
+++ pages/authentification.py
@@ -1,5 +1,5 @@
  # streamlit_app.py
-from operator import iconcat
+from operator import truediv
 import streamlit as st
 import mysql.connector
 
@@ -19,6 +19,17 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
+@st.experimental_memo(ttl=600)
+def run_query_insert(query):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+        return True
+    except:
+        return False
+    
+
 def check_password(rows, name, pwd):
     for row in rows:
         if(row[0] == name and row[1] == pwd):
@@ -28,8 +39,23 @@ def check_password(rows, name, pwd):
             return
     st.warning("Incorrect username/password")
 
-def insert_user(name, pseudo, mail, pwd):
-    print("hey ma couillasse")
+def verify_user(name, mail):
+    query = "SELECT * FROM user WHERE name = '" + name + "' OR mail = '" + mail + "'"
+    rows = run_query(query)
+    if(len(rows) != 0):
+        st.warning("This username or mail already exist")
+        return False
+    else:
+        return True
+
+def insert_user(name, mail, pwd):
+    if(verify_user(name, mail)):
+        query = "INSERT INTO user (name, mail, pwd) VALUES ('" + name + "', '" + mail + "', '" + pwd + "')"
+        succes = run_query_insert(query)
+        if(succes):
+            st.success("User created successfully")
+        else:
+            st.warning("User creation failed")
 
 st.title('Authentification')
 st.text('Veuillez vous authentifier pour accéder à la page product')
@@ -41,12 +67,11 @@ st.button("Connection", on_click=check_password, args=(rows, name, pwd))
 st.text('Si vous n\'avez pas de compte, veuillez vous inscrire')
 st.text('Veuillez renseigner les champs suivants')
 new_name = st.text_input("Name inscription")
-new_pseudo = st.text_input("Pseudo inscription")
 new_mail = st.text_input("Mail inscription")
 new_pwd = st.text_input("Password inscription", type="password")
 new_pwd2 = st.text_input("Repeat Password", type="password")
 
 if(new_pwd == new_pwd2 and new_pwd != "" and new_pwd2 != ""):
-    st.button("Inscription", on_click=insert_user, args=(new_name, new_pseudo, new_mail, new_pwd))
+    st.button("Inscription", on_click=insert_user, args=(new_name, new_mail, new_pwd))
 else:
     st.warning("Les mots de passe ne correspondent pas")
